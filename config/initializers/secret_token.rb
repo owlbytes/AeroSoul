0f3164c76c43fe793d0a8ce6c06b69a2eb7948db
owlbytes <neshah07@gmail.com>
2013-12-10 11:30:00 +0000

    created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtOfTheStreets::Application.config.secret_token = '98ab935138e8fdca69a727716a6869f4973416c9bc0d50246dc5a1d5d9f5061513072b36d7032334a4e635f76414565eabc33992de4b082220f53342b7d04d74'
