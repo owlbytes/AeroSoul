9f02911c7eaa5ce21b65e50bf173e0d283b97f38
owlbytes <neshah07@gmail.com>
2013-12-19 14:45:41 +0000

    downcased images extensions, fixed seed encoding error , added public/uploads to gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
-
+# encoding: UTF-8
 #creates a method so that rails can find, read, and show pictures for the index/show pages
 def read_asset(path)
-  File.read File.join(Rails.root, path)
+  File.open File.join(Rails.root, path)
 end
 
 #posts for all! 
-post1 = Post.create!(title: "Trashbags", artist: "Francisco de Pájaro", commision: "no", description: "interacting energetic figures made out of trash bags on the sidewalk", photo: read_asset("/app/assets/images/seeds/trashbags.jpg"), medium: "recycled materials", address: "1 Rivington Street, London, United Kingdom", tag_list: "Trashbags, recycled")
+post1 = Post.create!(title: "Trashbags", artist: "Francisco de Pejaro", commision: "no", description: "interacting energetic figures made out of trash bags on the sidewalk", photo: read_asset("/app/assets/images/seeds/trashbags.jpg"), medium: "recycled materials", address: "1 Rivington Street, London, United Kingdom", tag_list: "Trashbags, recycled")
 
 post2 = Post.create!(title: "Revolution", artist: "unknown", commision: "no", description: "Uncle Sam wants you to be a part of the revolution", photo: read_asset("/app/assets/images/seeds/revolution.jpg"), medium: "stencil and spray paint", address: " Christina Street, London, United Kingdom", tag_list: " Revolution, politics")
  
 post3 = Post.create!(title: "Hashtag of Ne", artist: "Neha Shah", commision: "no", description: "#Ne", photo: read_asset("/app/assets/images/seeds/hashtagneha.jpg"), medium: "stencil and spray paint", address: "3 Rivington Street, London, United Kingdom", tag_list: "tag, hashtag")
 
-post4 = Post.create!(title: "Animals", artist: "unknown", commision: "unknown", description: "Various animals heaped on top of each other", photo: read_asset("/app/assets/images/seeds/ranimal.jpg"), medium: "spray paint", address: "3 Rivington Street, London, United Kingdom", tag_list: "animals")
+post4 = Post.create!(title: "Animals", artist: "unknown", commision: "unknown", description: "Various animals heaped on top of each other", photo: read_asset("/app/assets/images/seeds/animal.jpg"), medium: "spray paint", address: "3 Rivington Street, London, United Kingdom", tag_list: "animals")
 
 post5 = Post.create!(title: "Cash Machine", artist: "Banksy", commision: "no", description: "Girl & an ATM machine. Has two white stripes covering part of the piece.", photo: read_asset("/app/assets/images/seeds/banksy.jpg"), medium: "spray paint", address: "114b Farringdon Road, London, United Kingdom", tag_list:" capitalism, politics, Banksy")
 
 post6 = Post.create!(title: "Bird & Snake", artist: "unknown", commision: "unknown", description: "A colorful scene featuring a bird and snake", photo: read_asset("/app/assets/images/seeds/bird_and_snake.jpg"), medium: "spray paint", address: "3 Rivington Street, London, United Kingdom", tag_list: "animals")
 
-post7 = Post.create!(title: "Skull", artist: "unknown", commision: "unknown", description: "A striking skull presides over Shoreditch", photo: read_asset("/app/assets/images/seeds/blackskull.jpg"), medium: "unknown", address: "3 Rivington Street, London, United Kingdom", tag_list: "skull, scultpure")
+post7 = Post.create!(title: "Skull", artist: "unknown", commision: "unknown", description: "A striking skull presides over Shoreditch", photo: read_asset("/app/assets/images/seeds/blacksull.jpg"), medium: "unknown", address: "3 Rivington Street, London, United Kingdom", tag_list: "skull, scultpure")
 
 post8 = Post.create!(title: "Buzz", artist: "unknown", commision: "unknown", description: "Cartoonish figures appear on Old Street, including Buzz Lightyear", photo: read_asset("/app/assets/images/seeds/buzz.jpg"), medium: "spray paint", address: "237 Old Street, London, United Kingdom", tag_list: "cartoons, Disney")
 
