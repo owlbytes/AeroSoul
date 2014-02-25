08564541cddd65d312a2d11219f0ba95e9db77f2
owlbytes <neshah07@gmail.com>
2014-02-25 16:51:15 +0000

    testing controllers

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,29 @@
+# require "spec_helper"
+
+# describe UsersController do 
+#   # creating tests to test the users controller
+#   it "should pass" do
+#     true
+#   end
+
+#   it "should fail" do
+#     false
+#   end
+
+#   it "should be pending"
+
+# #   creating fake data to test the index method of the users controller
+#   describe "GET index" do
+#     before do
+#       @users_fake_data = 2.times.map{User.create(first_name:"spray", last_name:"can", role:"user", email:"spray@spraycan.com", photo: nil, )}
+#     end
+
+#     it "should assigns @users" do
+#       get :index
+#       expect(assigns[:users]).to eq(@users_fake_data)
+#     end
+#   end
+
+
+# end
+  
