41359e9b50197b438be21432dc38274c4398616b
owlbytes <neshah07@gmail.com>
2014-03-12 20:16:46 +0000

    fixed oauth error'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,10 +43,10 @@ ArtOfTheStreets::Application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = { 
     address: "smtp.mandrillapp.com", 
-    port: 25, 
+    port: 587, 
     enable_starttls_auto: true, 
     user_name: ENV["MANDRILL_USERNAME"], 
-    password: ENV["MANDRILL_API_KEY"] 
+    password: ENV["AEROSOUL_MANDRILL_API_KEY"] 
 }
 
 end
