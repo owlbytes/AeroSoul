3115e9570e11bee60295110b23f22208d5f66188
owlbytes <neshah07@gmail.com>
2014-05-21 20:05:33 +0100

    reworking the ability model so that the app isn't confused

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,31 +4,43 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    if user.role == 'admin'
-      can :manage, :all
-    
-    elsif user.role == 'user'
+    can :read, :all
+    can :star, :all 
+    can :vote, :all  
+    can :create, Post
 
-      can :read, :all
-      can :star, :all
+    can :edit, Post do |post|
+      post.user == user
+    end
 
-      can :vote, :all
+    can :update, Post do |post|
+      post.user == user
+    end
 
-      can :create, Post
-      can :edit, Post do |post|
-        post.user == user
-      end
-      can :update, Post do |post|
-        post.user == user
-      end
-      can :destroy, Post do |post|
-        post.user == user
-      end
+    can :destroy, Post do |post|
+      post.user == user
+    end
 
-    else
+
+    if user.role == nil
       can :read, :all
       can :create, User
+    end 
 
-    end
-  end
-end
+    if user.role == "admin"
+      can :manage, :all
+    end 
+
+  end 
+
+
+
+
+
+
+    
+
+     
+
+
+       
