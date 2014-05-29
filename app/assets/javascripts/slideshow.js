9b18ba8dcda7683db2e5b994ef3cd5810cdaf53f
owlbytes <neshah07@gmail.com>
2014-05-29 16:06:51 +0100

    changed footer colors

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -4,7 +4,7 @@ jQuery(function($) {
   var $slider = $('.slider'); // class or id of carousel slider
   var $slide = 'li'; // could also use 'img' if you're not using a ul
   var $transition_time = 1000; // 1 second
-  var $time_between_slides = 4000; // 4 seconds
+  var $time_between_slides = 5000; // 4 seconds
 
   function slides(){
     return $slider.find($slide);
