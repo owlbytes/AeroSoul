7776489cb9dea690fb6430536939ccf17980af97
owlbytes <neshah07@gmail.com>
2014-06-04 23:51:21 +0100

    fixed heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,12 @@ ArtOfTheStreets::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-<<<<<<< HEAD
+
   config.action_mailer.default_url_options = {:host => "http://aerosoul.me/"}
-=======
-  config.action_mailer.default_url_options = { :host => 'example.com' }
->>>>>>> dev
-  # ActionMailer Config
-  # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 end
+
+
