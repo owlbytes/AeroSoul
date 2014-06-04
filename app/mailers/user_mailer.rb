317bf825e411540cf81fc47432d373af30a14d5b
owlbytes <neshah07@gmail.com>
2014-06-05 00:00:19 +0100

    fixed another heroku bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,11 @@
 class UserMailer < ActionMailer::Base
-<<<<<<< HEAD
-  default from: "neha@aerosoul.me"
 
-  def registration_confirmation user
-    @user = user
-    mail to:user.email, subject: "Confirmation: AeroSoul Registration" 
-  end
-    
-=======
   default from: "aerosoul@aerosoul.me"
 
   def registration_confirmation user
     @user = user
     mail to: user.email, subject: "Welcome to AeroSoul"
   end
->>>>>>> dev
+
 
 end
