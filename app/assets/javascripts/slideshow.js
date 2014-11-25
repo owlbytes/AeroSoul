2002dace572b478236248f94462eec8f2e8fa582
owlbytes <neshah07@gmail.com>
2014-11-25 19:27:54 +0000

    added full screen slideshow to home page

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,35 +1,35 @@
-jQuery(function($) { 
+// jQuery(function($) { 
 
-  // settings
-  var $slider = $('.slider'); // class or id of carousel slider
-  var $slide = 'li'; // could also use 'img' if you're not using a ul
-  var $transition_time = 9000; // 1 second
-  var $time_between_slides = 9000; // 4 seconds
+//   // settings
+//   var $slider = $('.slider'); // class or id of carousel slider
+//   var $slide = 'li'; // could also use 'img' if you're not using a ul
+//   var $transition_time = 9000; // 1 second
+//   var $time_between_slides = 9000; // 4 seconds
 
-  function slides(){
-    return $slider.find($slide);
-  }
+//   function slides(){
+//     return $slider.find($slide);
+//   }
 
-  slides().fadeOut();
+//   slides().fadeOut();
 
-  // set active classes
-  slides().first().addClass('active');
-  slides().first().fadeIn($transition_time);
+//   // set active classes
+//   slides().first().addClass('active');
+//   slides().first().fadeIn($transition_time);
 
-  // auto scroll 
-  $interval = setInterval(
-    function(){
-      var $i = $slider.find($slide + '.active').index();
+//   // auto scroll 
+//   $interval = setInterval(
+//     function(){
+//       var $i = $slider.find($slide + '.active').index();
 
-      slides().eq($i).removeClass('active');
-      slides().eq($i).fadeOut($transition_time);
+//       slides().eq($i).removeClass('active');
+//       slides().eq($i).fadeOut($transition_time);
 
-      if (slides().length == $i + 1) $i = -1; // loop to start
+//       if (slides().length == $i + 1) $i = -1; // loop to start
 
-      slides().eq($i + 1).fadeIn($transition_time);
-      slides().eq($i + 1).addClass('active');
-    }
-    , $transition_time +  $time_between_slides 
-  );
+//       slides().eq($i + 1).fadeIn($transition_time);
+//       slides().eq($i + 1).addClass('active');
+//     }
+//     , $transition_time +  $time_between_slides 
+//   );
 
-});
+// });
