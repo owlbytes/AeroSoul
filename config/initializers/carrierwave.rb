8fcb9a0c451b201ccb52131f5e0e7fabcfecb25b
owlbytes <neshah07@gmail.com>
2014-12-13 14:32:28 +0000

    styling for tag buttons

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider  => 'AWS',  # required
     :aws_access_key_id  => ENV['AWS_ACCESS_KEY_ID'],  # required
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],  # required
-    :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
+    # :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = ENV['Bina_BUCKET']  # required
 
