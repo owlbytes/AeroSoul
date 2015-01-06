8d06c54c6067488e3c8d70237202a1e69c0f1b34
owlbytes <neshah07@gmail.com>
2015-01-06 18:43:32 +0000

    mispelled my bucket name :(

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider  => 'AWS',  # required
-    :aws_access_key_id  => ENV['AWS_ACCESS_KEY_ID'],  # required
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],  # required
-    # :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
+    :aws_access_key_id  => ENV['AWSAccessKeyId'],  # required
+    :aws_secret_access_key => ENV['AWSSecretKey'],  # required
+    :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['Bina_BUCKET']  # required
+  config.fog_directory  = ENV['AeroBucket']  # required
 
   config.fog_public  = true  # optional, defaults to true
 end
