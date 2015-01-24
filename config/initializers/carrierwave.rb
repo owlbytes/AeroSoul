0623a8c80440eea790ac14138ad0cbfc3781f79f
owlbytes <neshah07@gmail.com>
2015-01-24 18:35:55 +0000

    styling of login buttons

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,7 @@ CarrierWave.configure do |config|
     :provider  => 'AWS',  # required
     :aws_access_key_id  => ENV['AWSAccessKeyId'],  # required
     :aws_secret_access_key => ENV['AWSSecretKey'],  # required
-<<<<<<< HEAD
-    :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
-=======
-    # :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
->>>>>>> bac36c02a77cc4bd3c345637301e35b08d0ef629
+    :region => 'eu-west-1',  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = ENV['AeroBucket']  # required
 
