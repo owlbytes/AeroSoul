04a908469241b22e0cc8ca1b4d7c74a0fe752627
owlbytes <neshah07@gmail.com>
2015-02-23 18:54:06 +0000

    updating config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ ArtOfTheStreets::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => "http://aerosoul.me/"}
+  config.action_mailer.default_url_options = { :host => "aerosoul.me"}
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.perform_deliveries = true 
   config.action_mailer.raise_delivery_errors = true 
