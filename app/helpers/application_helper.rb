896a184efda65e0bd6a025810dc3ca467f96ba5a
owlbytes <neshah07@gmail.com>
2015-02-24 23:02:30 +0000

    updating application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def randomized_background_image
+    images = ["/assets/images/forms/1.jpg", "/assets/images/forms/2.jpg", "/assets/images/forms/1.jpg"].shuffle
+  end
 end
